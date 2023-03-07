50c58890d19bc7cba4fb5d1e79d9ada5d2b34af9
Abhishek <abhishekparmar162@gmail.com>
2023-03-07 13:01:24 +0530

    added right section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
-import {BiSearchAlt2} from 'react-icons/bi'
+import {BiSearchAlt2,BiMenu} from 'react-icons/bi'
+import {FiGlobe} from 'react-icons/fi'
+import {FaUserCircle} from 'react-icons/fa'
 
 const Header = () => {
   return (
@@ -10,11 +12,18 @@ const Header = () => {
       </div>
       {/* Middle */}
       <div className='flex items-center md:border-2 rounded-full p-1 border-gray-400 md:shadow-sm'>
-        <input  placeholder='start your search ..' className='flex-grow outline-none bg-transparent ml-3 placeholder:text-gray-400 text-gray-600'/>
+        <input  placeholder='start your search ..' className='flex-grow text-xs md:text-sm outline-none bg-transparent ml-3 placeholder:text-gray-400 text-gray-600'/>
         <BiSearchAlt2 size={20} className='bg-red-500 text-white rounded-full p-1 hidden md:inline-flex cursor-pointer' />
       </div>
       {/* right */}
-      <div></div>
+      <div className='flex items-center justify-end md:space-x-4 space-x-2 text-gray-500'>
+        <div><p className='hidden md:inline'>Airbnd your home</p></div>
+        <div><FiGlobe /></div>
+        <div className='flex border-2 rounded-full p-1 space-x-2'> 
+          <BiMenu size={20} />
+          <FaUserCircle size={20} />
+        </div>
+      </div>
     </header>
   )
 }
