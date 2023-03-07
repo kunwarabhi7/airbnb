0a98e396a5fa1fa04e55e670f0a3face63f0334a
Abhishek <abhishekparmar162@gmail.com>
2023-03-07 13:03:29 +0530

    start banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 const Home= () => {
   return (
     <>
@@ -8,7 +9,7 @@ const Home= () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 <Header />
-     {/* Banner */}
+     <Banner />
 
     </>
   )
