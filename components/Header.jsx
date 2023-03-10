f24bd69d88cbfc2d44d745e1231c2444bb995650
Abhishek <abhishekparmar162@gmail.com>
2023-03-10 14:54:44 +0530

    added calender

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,27 @@ import Image from 'next/image'
 import {BiSearchAlt2,BiMenu} from 'react-icons/bi'
 import {FiGlobe} from 'react-icons/fi'
 import {FaUserCircle} from 'react-icons/fa'
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
 
 const Header = () => {
+  const [input, setInput] = useState("")
+const [startDate, setStartDate] = useState(new Date())
+const [endDate, setEndDate] = useState(new Date())
+  const selectionRange = {
+    startDate ,
+    endDate ,
+    key: 'selection',
+  }
+
+  const handleDate = (ranges) => {
+setStartDate(ranges.selection.startDate);
+setEndDate(ranges.selection.endDate);
+  }
+
   return (
     <header className=' sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* Left */}
@@ -12,7 +31,7 @@ const Header = () => {
       </div>
       {/* Middle */}
       <div className='flex items-center md:border-2 rounded-full p-1 border-gray-400 md:shadow-sm'>
-        <input  placeholder='start your search ..' className='flex-grow text-xs md:text-sm outline-none bg-transparent ml-3 placeholder:text-gray-400 text-gray-600'/>
+        <input value={input} onChange={e=>setInput(e.target.value)}  placeholder='start your search ..' className='flex-grow text-xs md:text-sm outline-none bg-transparent ml-3 placeholder:text-gray-400 text-gray-600'/>
         <BiSearchAlt2 size={20} className='bg-red-500 text-white rounded-full p-1 hidden md:inline-flex cursor-pointer' />
       </div>
       {/* right */}
@@ -24,6 +43,9 @@ const Header = () => {
           <FaUserCircle size={20} />
         </div>
       </div>
+     {input && <div>
+      <DateRangePicker ranges={[selectionRange]} minDate={new Date()} onChange={handleDate} rangeColors={["#FD5B61"]} />
+      </div>}
     </header>
   )
 }
