56e22bb87c57cbd6b7f58230d54a6fa34b97ebe4
Abhishek <abhishekparmar162@gmail.com>
2023-03-10 13:03:44 +0530

    added footert

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-400 h-40'>
-      Footer
+    <div className='bg-gray-400  h-44 md:py-3 my-2 text-xs md:text-sm'>
+        <div className='flex'>
+
+       
+      <div className='p-2 max-w-6xl mx-auto'>
+        <h1 className='font-semibold text-xs md:text-lg'>About</h1>
+        <p className='text-gray-700'>How AirBnd Works</p>
+        <p className='text-gray-700'>Newsroom</p>
+        <p className='text-gray-700'>Investors</p>
+        <p className='text-gray-700'>AirBnb Plus</p>
+        <p className='text-gray-700'>AirBnd Luxe </p>
+      </div>
+ 
+      <div className='p-2 max-w-6xl mx-auto'>
+        <h1 className='font-semibold text-xs md:text-lg'>COMMUNITY</h1>
+        <p className='text-gray-700'>Accessiblity</p>
+        <p className='text-gray-700'>This is not a real site</p>
+        <p className='text-gray-700'>Its a pretty awesome clone.</p>
+        <p className='text-gray-700'>Referrals accepted</p>
+        <p className='text-gray-700'>yo!Bro!</p>
+      </div> 
+      <div className='p-2 max-w-6xl mx-auto'>
+        <h1 className='font-semibold text-xs md:text-lg'>SUPPORT</h1>
+        <p className='text-gray-700'>Help Centre</p>
+        <p className='text-gray-700'>Trust & Safety</p>
+        <p className='text-gray-700'>Say Hi YouTube</p>
+        <p className='text-gray-700'>Easter Eggs</p>
+        <p className='text-gray-700'>For  Win</p>
+      </div> 
+      <div className='p-2 max-w-6xl mx-auto'>
+        <h1 className='font-semibold text-xs md:text-lg'>HOST</h1>
+        <p className='text-gray-700'>Abhishek</p>
+        <p className='text-gray-700'>Gifts</p>
+        <p className='text-gray-700'>Promo Code</p>
+        <p className='text-gray-700'>Follow ME</p>
+        <p className='text-gray-700'>Twitter</p>
+      </div>
+      </div>
     </div>
   )
 }
