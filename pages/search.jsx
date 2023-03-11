994a3ece508e3f4dac603623f7ce3f135974f294
Abhishek <abhishekparmar162@gmail.com>
2023-03-11 12:06:47 +0530

    add query location

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,17 +1,20 @@
+import { useRouter } from "next/router";
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Search = () => {
+ const router =  useRouter()
+ console.log(router.query)
   return (
     <div className="">
       <Header />
       <main>
         <section className="flex-grow pt-14 px-6">
           <p className="text-sm text-gray-500">
-            300+ Stays for 5 number of guests
+            300+ Stays for {router.query.numberOfGuest} number of guests
           </p>
-          <h1 className="text-2xl font-semibold">Stays in Bhiwani</h1>
+          <h1 className="text-2xl font-semibold">Stays in {router.query.location}</h1>
           <div className="flex flex-col space-y-2 md:flex-row md:space-x-6 mx-auto">
             <button className="button ">Cancellation Flexiblity</button>
             <button className="button ">Types of place</button>
